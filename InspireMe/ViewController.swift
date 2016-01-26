57ea75bc4ae03c6a4fa0fcfc72ec5db634d44434
Dante Solorio <dasoga@dantes-macbook-air.local>
2016-01-26 17:01:15 -0600

    Initial Commit

---
+++ InspireMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InspireMe
+//
+//  Created by Dante Solorio on 1/26/16.
+//  Copyright © 2016 Zahui Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
